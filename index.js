476b18af844ee688b81357ef845668eaf25ef8b4
221179 <estelle.abebanga@sncf.fr>
2022-08-25 10:36:31 +0200

    src

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const Port = process.env.PORT;
-const { Welcome } = require('./routes.js');
+const { Welcome } = require('./src/routes.js);
 
 app.get('/', Welcome);
 
